3f1aa411915ba21ae8e3aa3982424060317c8981
Andrey Bulat <andrii.bulat@gmail.com>
2016-09-26 19:59:52 +0300

    Added first lab

---
+++ 1/src/HelloWorldApplet.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Andrew on 9/26/16.
+ */
+import java.applet.*;
+import java.awt.*;
+
+public class HelloWorldApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello world! It's my first Java Applet application!", 25, 50);
+    }
+}
